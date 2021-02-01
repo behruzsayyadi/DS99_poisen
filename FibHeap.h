b43c52e18ce2265f4ecf43170d09401d932b6835
BehruzSayyadi <60942549+behruzsayyadi@users.noreply.github.com>
2021-02-01 03:35:47 +0330

    defender soldier class changed into struct

--- FibHeap.h
+++ FibHeap.h
@@ -1,35 +1,16 @@
 #pragma once
 #include "AVL_BST.h"
 
-class defender_soldier_node
+struct defender_soldier_node
 {
-public:
-    soldier_node* sld_node;
+    int sld_power;
     int degree;
     defender_soldier_node* parent;
     defender_soldier_node* child;
     defender_soldier_node* leftSibling;
     defender_soldier_node* rightSibling;
     bool mark;
-    defender_soldier_node()
-        : parent(nullptr)
-        , child(nullptr)
-        , leftSibling(nullptr)
-        , rightSibling(nullptr)
-        , degree(0)
-        , mark(false)
-    {
-        sld_node = new soldier_node();
-    }
-    //defender_soldier_node(soldier_node* sldr)
-    //{
-    //    sld_node = sldr;
-    //    parent = nullptr;
-    //    child = nullptr;
-    //    leftSibling = nullptr;
-    //    rightSibling = nullptr;
-    //}
-    
+    char C;
 };
 
 class FiboHeap
@@ -47,9 +28,6 @@ public:
 
 FiboHeap::FiboHeap()
 {
-    this->head = new defender_soldier_node();
-    this->head->parent = nullptr;
-    this->head->child = nullptr;
 }
 
 FiboHeap::~FiboHeap()
