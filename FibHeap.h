b37d52b6288901e56b9571a0f3d7b4a1f7f185c5
BehruzSayyadi <60942549+behruzsayyadi@users.noreply.github.com>
2021-02-02 05:59:43 +0330

    80 percent fibo heap not debugged

--- FibHeap.h
+++ FibHeap.h
@@ -23,13 +23,15 @@ public:
 
 
     void Insert(int key,bool fight);
-    void mergeToCircute(defender_soldier_node* our_node, defender_soldier_node* circute_node);
+    defender_soldier_node* mergeToCircute(defender_soldier_node* our_node, defender_soldier_node* circute_node);
     defender_soldier_node* childCircute(defender_soldier_node* t);
 
     defender_soldier_node* popMin();
     void delete_no_fight();
     void remake();
     void makeChild(defender_soldier_node* child, defender_soldier_node* parent);
+
+    defender_soldier_node* removeNode(defender_soldier_node* node);
 };
 
 FiboHeap::FiboHeap()
@@ -82,7 +84,7 @@ inline void FiboHeap::Insert(int key,bool fight)
 
 }
 
-inline void FiboHeap::mergeToCircute(defender_soldier_node* our_node, defender_soldier_node* circute_node)
+inline defender_soldier_node* FiboHeap::mergeToCircute(defender_soldier_node* our_node, defender_soldier_node* circute_node)
 {
     //if (t->leftSibling == t)
     //{
@@ -97,16 +99,21 @@ inline void FiboHeap::mergeToCircute(defender_soldier_node* our_node, defender_s
     //    this->min_node->leftSibling->rightSibling = t;
     //    circute_node->min_node->leftSibling = t;
     //}
+    if (our_node == nullptr || circute_node == nullptr)
+        return nullptr;
     defender_soldier_node* a;
     a = our_node->leftSibling;
     circute_node->leftSibling->rightSibling = our_node;
     our_node->leftSibling = circute_node->leftSibling;
     a->rightSibling = circute_node;
     circute_node->leftSibling = a;
+    return circute_node;
 }
 
 inline defender_soldier_node* FiboHeap::childCircute(defender_soldier_node* t)
 {
+    if (t == nullptr)
+        return nullptr;
     defender_soldier_node* circute = t;
 
     circute->parent = nullptr;
@@ -134,7 +141,38 @@ inline defender_soldier_node* FiboHeap::popMin()
 
 inline void FiboHeap::delete_no_fight()
 {
+    bool flag = false;
+    
+    defender_soldier_node* min = this->min_node;
+    while (true)
+    {
+        if (min->fighter)
+        {
+            min = min->rightSibling;
+        }
+        else 
+        {
+            break;
+        }
 
+    }
+    if (min == min->leftSibling)
+        flag = true;
+    if (min != nullptr)
+    {
+        childCircute(min->child);
+        mergeToCircute(min->child, min);
+        removeNode(min);
+        if (flag)
+            return;
+        else
+        {
+            this->min_node = min->leftSibling;
+            this->remake();
+        }
+        this->num_of_nodes--;
+    }
+    return;
 }
 
 inline void FiboHeap::remake()
@@ -147,14 +185,15 @@ inline void FiboHeap::remake()
 
     defender_soldier_node* iter = this->min_node;
     
-    //bool break_flag = false;
-    while (true)
+    bool break_flag = false;
+    /*while (true)
     {
+        
+        iter = iter->rightSibling;
         if (check_arr[iter->degree] == nullptr)
         {
             check_arr[iter->degree] = iter;
 
-            iter = iter->rightSibling;
         }
         else
         {
@@ -166,16 +205,61 @@ inline void FiboHeap::remake()
             iter = min_node;
         }
         
+    }*/
+    while (true)
+    {
+        int d = iter->degree;
+        while (check_arr[d] != nullptr)
+        {
+            defender_soldier_node* y = check_arr[d];
+            if (y == iter)
+            {
+                break_flag = true;  
+                break;			   
+            }
+            if (iter->sld_power > y->sld_power)  
+                                
+            {
+                defender_soldier_node* temp = iter;
+                iter = y;
+                y = temp;
+            }
+            makeChild(y, iter); 
+            check_arr[d++] = nullptr;   
+        }
+        if (break_flag)
+            break;
+        check_arr[iter->degree] = iter;
+        iter = iter->rightSibling;
     }
-    
+    this->min_node = iter; 
+    defender_soldier_node* iter_ = iter;
+    do {
+        if (iter_->sld_power < min_node->sld_power)
+            min_node = iter_;
+        iter_ = iter_->rightSibling;
+    } while (iter_ != iter);
+    delete[]check_arr;
 }
 
 inline void FiboHeap::makeChild(defender_soldier_node* child, defender_soldier_node* parent)
 {
-    defender_soldier_node* child_right, * child_left;
-    child_right = child->rightSibling;
-    child_left = child->leftSibling;
-    child_left = child_right;
-    child_right = child_left;
+    removeNode(child);
+    child->leftSibling = child->rightSibling = child;
+    child->parent = parent;
+    parent->child = mergeToCircute(child, parent->child);
+    parent->degree++;
+    child->mark = false;// hmmm.....
+}
+
+
+inline defender_soldier_node* FiboHeap::removeNode(defender_soldier_node* node)
+{
+    defender_soldier_node* node_right, * node_left;
+    node_right = node->rightSibling;
+    node_left = node->leftSibling;
+    node_left = node_right;
+    node_right = node_left;
+    return node;
 }
 
